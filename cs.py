bce685b6d8fa47e1b6ab43534078dfb159c492d8
Jose20224 <153401082+Jose20224@users.noreply.github.com>
2024-07-08 17:21:50 -0600

    Update cs.py

--- cs.py
+++ cs.py
@@ -1,6 +1,6 @@
 import pyodbc
 
 def Con():
-    cnxn_str = 'DRIVER={SQL Server};SERVER=JOSE\\SQLEXPRESS;DATABASE=Proyecto_CS50_1;Trusted_Connection=yes'
+    cnxn_str = 'DRIVER={SQL Server};SERVER=(Nombre del servidor);DATABASE=Proyecto_CS50_1;Trusted_Connection=yes'
     cnxn = pyodbc.connect(cnxn_str)
     return cnxn
